1d5f56318595cd2ce81cffff1ce6ae9e58e279c2
Gunjan Agarwal <gunjanagarwal@Gunjans-MBP.bbrouter>
2022-08-15 18:43:08 +0530

    [Gunjan] Add. calculate price of 500g onion 1kg apple and 3kg rice

--- src/test/java/DailyNeedTest.java
+++ src/test/java/DailyNeedTest.java
@@ -21,11 +21,24 @@ public class DailyNeedTest {
     @Test
     void shouldEquateToPriceFiveHundredFortyRupeesFor1kgKiwi2kgAppleAnd3kgTomato(){
         DailyNeed products = new DailyNeed(Arrays.asList(new Product(Fruits.kiwi ,1),
-                new Product(Fruits.apple ,2), new Product(Vegetable.tomato ,3)));
+                new Product(Fruits.apple ,2),
+                new Product(Vegetable.tomato ,3)));
 
         double totalPrice = products.totalPrice();
 
         assertEquals(540,totalPrice);
+
+    }
+
+    @Test
+    void shouldEquateToPriceFiveHundredFortyRupeesFor500gOnion1kgAppleAnd3kgRice(){
+        DailyNeed products = new DailyNeed(Arrays.asList(new Product(Vegetable.onion ,0.5),
+                new Product(Fruits.apple ,1),
+                new Product(Grocery.rice ,3)));
+
+        double totalPrice = products.totalPrice();
+
+        assertEquals(313.5,totalPrice);
 
     }
 
