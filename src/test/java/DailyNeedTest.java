4b244feb1649dbc63dedbed6d4a51fd552b6dde0
Gunjan Agarwal <gunjanagarwal@Gunjans-MBP.bbrouter>
2022-08-15 18:38:02 +0530

    [Gunjan] Add. calculate price of 1kg kiwi 2kg apple and 3kg tomato

--- src/test/java/DailyNeedTest.java
+++ src/test/java/DailyNeedTest.java
@@ -18,4 +18,15 @@ public class DailyNeedTest {
 
     }
 
+    @Test
+    void shouldEquateToPriceFiveHundredFortyRupeesFor1kgKiwi2kgAppleAnd3kgTomato(){
+        DailyNeed products = new DailyNeed(Arrays.asList(new Product(Fruits.kiwi ,1),
+                new Product(Fruits.apple ,2), new Product(Vegetable.tomato ,3)));
+
+        double totalPrice = products.totalPrice();
+
+        assertEquals(540,totalPrice);
+
+    }
+
 }
